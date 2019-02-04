fb7b52d517546ca68cf10ff8cf5865868802746c
codevel1 <c.tomazinho@gmail.com>
2019-02-04 10:37:33 -0200

    ending chatbot example

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ nltk.download('wordnet') #first time use only
 sent_tokens = nltk.sent_tokenize(raw) #convert to list of sentences
 word_tokens = nltk.word_tokenize(raw) # convert to list of words
 
-print(sent_tokens[:2])
+#print(sent_tokens[:2])
 
 lemmer=nltk.stem.WordNetLemmatizer()
 
@@ -42,4 +42,42 @@ def greeting(sentence):
         if word.lower() in greetings_input:
             return random.choice(gretings_response)
 
-print('test')
+robo_response=''
+
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+flag=True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")
